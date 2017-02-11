07595afa955dd7baa4e92d3d9a02639936ffaf36
Kah Hwee Gan <hwee28687@gmail.com>
2017-02-12 01:17:18 +0800

    Refined UI, bug fixes
    Added household cleaning, party, speech, suggestions
    Bugs: email address, gender

--- js/scripts.js
+++ js/scripts.js
@@ -3,82 +3,148 @@ w3IncludeHTML();
 var steps = $(".step"), bootstrapSlider, trashContainer;
 
 setTimeout(function(){
-    $("div.step.hidden").removeClass("hidden");
-    
-    $('input.check_radio').iCheck({
-    	checkboxClass: 'icheckbox_square-aero',
-   	    radioClass: 'iradio_square-aero'
+    $.getScript( "js/functions.js", function(){
+        $('.gender-radio').removeClass("hidden");
+        $("div.step.hidden").removeClass("hidden");
     });
 },1000);
+setTimeout(function(){
+    $.getScript( "js/quantity-bt.js");
+},2000);
 
 function processResults(){
     trashContainer = $("#trash");
     
     console.log("all steps completed");
     trashContainer.children().remove();
-    var totalWeight = 0, data = {};
+    var totalWeight = 0, totalQuantity = 0, byoSaveWeight = 0, data = {};
     data.entry = {
         name: $("input[name=nickname]").val() + "," +
             $("input[name=age]").val() + "," +
-            $("input[name=gender]").val() + "," +
-            $("input[name=country]").val() + "," +
-            $("select[name=occupation]").val()
+            $("input[name=gender]:checked").val() + "," +
+            $("input[name=country]").val() + 
+            ($("select[name=occupation]").val() ? ("," + $("select[name=occupation]").val()) : ""),
+        email: $("input[name=email]").val()
     };
+    console.log(data.entry);
     data.values = [];
     for(var i=0; i<steps.length; i++){
         var step = $(steps[i]),
             durationSelect = step.find("select[name=duration]"),
-            numberInput = step.find("input[name=quantity][type=text]");
+            numberInput = step.find("input.qty[type=text]");
         if(durationSelect.length > 0){
             //console.log(durationSelect.val());
             //console.log("weights");
-            var toYear = durationSelect.val(),
+            var toYear = parseFloat(durationSelect.val()),
                 division = numberInput.val();
                 sliders = step.find("input.bootstrapSlider");
             for(var j=0; j<sliders.length; j++){
                 var slider = $(sliders[j]),
                     itemName = slider.parent().find(".lead").text(),
                     selectedQuantity = parseInt(slider.val()),
-                    totalQuantity = Math.round(selectedQuantity * toYear / division),
-                    weight = slider.attr("data-weight"),
+                    quantityPerYear = Math.round(selectedQuantity * toYear / division),
+                    weight = parseFloat(slider.attr("data-weight")),
                     img = slider.attr("data-img");
                 if(slider.attr("data-parent")=="true"){
                     itemName = slider.parent().parent().find(".lead").first().text() + " - " + itemName;
                 }
                 //console.log("- " + slider.attr("data-weight") + " x " + slider.val());
-                totalWeight += (weight * totalQuantity);
                 if(selectedQuantity > 0){
+                    
+                    var totalItemWeight = (weight * quantityPerYear);
+                    totalWeight += totalItemWeight;
+                    totalQuantity += quantityPerYear;
+                    
+                    var byo = slider.attr("data-byo");
+                    if(byo){
+                        byoSaveWeight += totalItemWeight;
+                    }
+                    var remark = selectedQuantity + " x " + toYear + " / " + division + " x " + weight + "g = " + 
+                                 quantityPerYear + " x " + weight + "g = " + Math.round(weight * quantityPerYear)/1000 + "kg";
                     if(img){
-                        trashContainer.append("<tr><td><img src='img/plastic-items/"+img+"' height='100px'></td><td><h4>"+itemName+"</h4></td><td><h2 style='min-width:120px;'>X "+totalQuantity+"</h2></td></tr>");
+                        trashContainer.append("<tr><td><img src='img/plastic-items/"+img+"' height='100px'></td>"+
+                                              "<td style='min-width:200px;'><h4>"+itemName+"</h4></td>"+
+                                              "<td style='min-width:120px;'><h2>X "+quantityPerYear+"</h2></td>"+
+                                              "<td style='min-width:120px;' class='trash-remark'>"+remark+
+                                              ("<br/>("+(totalWeight/1000)+"kg accummulated)</td>")+
+                                              "<td style='min-width:120px;' class='suggestion'>" +
+                                                (byo ? ("<h3 class='byo'>BYO<br/><strong>"+byo+"</strong>") : "") +
+                                                (slider.attr("data-refill") ? ("<h3 class='refill'>Choose<br/><strong>REFILLS</strong>") : "") +
+                                                (slider.attr("data-refuse") ? ("<h3 class='refuse'><strong>REFUSE</strong>") : "") +
+                                                (slider.attr("data-recycle") ? ("<h3 class='recycle'><strong>RECYCLE</strong>") : "") +
+                                              "</h3></td>"+
+                                              "</tr>");
                     }
                     data.values.push({
                         key: itemName,
-                        value: totalQuantity
+                        value: quantityPerYear,
+                        remark: remark
                     });
                 }
             }
         }
     }
-    var totalWeightKg = Math.round(totalWeight)/1000;
-    $("#totalWeight").text(totalWeightKg + " KG");
-    console.log("total = " + totalWeightKg);
+    
+    if(totalWeight > 0){
+        trashContainer.prepend("<tr style='background: #eee;'><th></th>"+
+                              "<th></th>"+
+                              "<th></th>"+
+                              "<th class='trash-remark'></th>"+
+                              "<th><h3>Suggestion:</h3></th>"+
+                              "</tr>");
+    }
+    
+    var totalWeightKg = Math.round(totalWeight/100)/10;
+    $("#totalWeight").html("<u>"+totalWeightKg + "</u> KG");
+    console.log("total weight = " + totalWeightKg);
+    
+    $("#totalQuantity").html("<u>"+totalQuantity + "</u> items");
+    console.log("total quanity = " + totalQuantity);
+    
     data.values.push({
         key: "totalWeightPerYearKg",
         value: totalWeightKg
     });
+    data.values.push({
+        key: "totalQuantityPerYear",
+        value: totalQuantity
+    });
     
     $(".mascott").hide(); 
+    $(".speech").hide(); 
+    
+    $("#trashTitle").show();
     if(totalWeightKg == 0){
         $("#trashTitle").hide();
-    } else {
-        $("#trashTitle").show();
     }
-    if(totalWeightKg < 10){
+    
+    $(".byo").show();
+    $(".byo h2").text(Math.round(byoSaveWeight/100)/10 + " KG");
+    if(byoSaveWeight == 0){
+        $(".byo").hide();
+    }
+    
+    if(totalWeightKg < 5 && totalQuantity < 200){
         $(".mascott.good").show(); 
-    } else if(totalWeightKg < 50){
+        $(".speech-5").show();
+        
+    } else if(totalWeightKg < 20 && totalQuantity < 600){
         $(".mascott.ok").show(); 
+        
+        if(totalWeightKg < 10 && totalQuantity < 400){
+            $(".speech-4").show();
+        } else {
+            $(".speech-3").show();
+        }
+        
     } else {
-        $(".mascott.bad").show(); 
+        if(totalWeightKg < 30 && totalQuantity < 800){
+            $(".mascott.bad").show(); 
+            $(".speech-2").show();
+        } else {
+            $(".mascott.worst").show(); 
+            $(".speech-1").show();
+        }
     }
     
     $.ajax({
@@ -91,6 +157,7 @@ function processResults(){
         for(var i=0;i<data.values.length;i++){
             data.values[i].entry_id = response;
         }
+        console.log(data.values);
         $.ajax({
             url: "/api/api.php/key_values",
             method: "POST",
